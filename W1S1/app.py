9d12d4a9de2ec2c4d3185dda4a77345257a5f66c
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-10-26 04:01:51 +0300

    W1S1

---
+++ W1S1/app.py
@@ -0,0 +1,14 @@
+# eslint-disable semi
+print('Hello World!!')
+# snake_case  NOT camelCase
+first_name = "test"
+age = 18
+is_instructor = True
+# THIS WILL NOT WORK!!!
+# print(first_name + age)
+
+print(first_name + str(age))
+# console.log(`The age is ${age}`)
+print(f"The first name is {first_name} and the age is {age}")
+
+print(type(first_name))
